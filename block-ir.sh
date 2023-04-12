c683610c47855423be74cc61197c974653ee16da
AC-Lover <49290111+AC-Lover@users.noreply.github.com>
2023-04-12 17:15:36 +0200

    Create block-ir.sh

---
+++ block-ir.sh
@@ -0,0 +1,37 @@
+sudo apt-get install jq -y
+
+
+cp /root/marzban/xray_coinfg.json /root/marzban/xray_coinfg.json.bak
+
+export xray=$(cat /root/marzban/xray_coinfg.json | jq '.routing.domainStrategy = "IPIfNonMatch"')
+
+export xray=$(echo "$xray" | jq '.routing.rules[0].ip += ["geoip:ir"]')
+
+export xray=$(echo "$xray" | jq '.routing.rules += [{"outboundTag": "blackhole", "domain": ["regexp:.*\\.ir$", "ext:iran.dat:ir", "ext:iran.dat:other", "geosite:category-ir", "blogfa", "bank", "tebyan.net", "beytoote.com", "Film2movie.ws", "Setare.com", "downloadha.com", "Sanjesh.org"], "type": "field"}]' )
+
+echo $(echo "$xray" | jq '.outbounds |= map(if .protocol == "blackhole" then .tag = "blackhole" else . end)') > /root/marzban/xray_coinfg.json
+
+mkdir -p /var/lib/marzban/assets/
+
+echo 'XRAY_ASSETS_PATH="/var/lib/marzban/assets/"' >> /root/marzban/env
+
+cat >/root/download-dats.sh <<EOL
+wget -O /var/lib/marzban/assets/geosite.dat https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat
+
+wget -O /var/lib/marzban/assets/geoip.dat https://github.com/v2fly/geoip/releases/latest/download/geoip.dat
+
+wget -O /var/lib/marzban/assets/iran.dat https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/iran.dat
+
+EOL
+
+{ crontab -l -u root; echo "0 */4 * * * /bin/bash /root/download-dats.sh >/dev/null 2>&1"; } | crontab -u root -
+
+bash /root/download-dats.sh
+
+cd /root/marzban
+
+docker compose down
+sleep 3
+docker compose up -d
+
+echo -e "\nDone\n"
